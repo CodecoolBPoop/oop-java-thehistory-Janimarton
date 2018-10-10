4bf7ea53e0b1ec58e2e2339b5093acc6fc631f69
Jani <janimarton22@gmail.com>
2018-10-10 12:12:37 +0200

    in the Array file: add, remove word, size, clear, replace one wordare implemented

--- src/main/java/com/codecool/thehistory/TheHistoryArray.java
+++ src/main/java/com/codecool/thehistory/TheHistoryArray.java
@@ -1,38 +1,62 @@
 package com.codecool.thehistory;
 
 import java.util.Arrays;
+import java.util.regex.Pattern;
 
 public class TheHistoryArray implements TheHistory {
 
     /**
      * This implementation should use a String array so don't change that!
      */
-    private String[] wordsArray = new String[0];
+    private String[] wordsArray;
 
     @Override
     public void add(String text) {
         //TODO: check the TheHistory interface for more information
+        String[] splitedString = text.split("\\s+");
+        wordsArray = new String[splitedString.length];
+        for (int i = 0; i < wordsArray.length; i++) {
+            wordsArray[i]=splitedString[i];
+        }
+
     }
 
     @Override
     public void removeWord(String wordToBeRemoved) {
         //TODO: check the TheHistory interface for more information
+        for (int i = 0; i < wordsArray.length; i++) {
+            if (wordsArray[i] == wordToBeRemoved) {
+                wordsArray[i] = "";
+            }
+        }
     }
 
     @Override
     public int size() {
         //TODO: check the TheHistory interface for more information
-        return 0;
+        int sizeNumber =0;
+        for (int i =0; i<wordsArray.length; i++){
+            sizeNumber = sizeNumber + 1;
+        }
+
+        return sizeNumber;
     }
 
     @Override
     public void clear() {
         //TODO: check the TheHistory interface for more information
+        wordsArray = null;
     }
 
     @Override
     public void replaceOneWord(String from, String to) {
         //TODO: check the TheHistory interface for more information
+        for (int i =0; i<wordsArray.length; i++){
+            if (wordsArray[i] == from){
+                wordsArray[i] = to;
+            }
+        }
+
     }
 
     @Override
